222f75df75106cb60eb408cbbd2188df8f3aa1bb
Abkarinohk <68717873+Abkarinohk@users.noreply.github.com>
2020-07-24 03:33:18 +0200

    Create kirolos.sh

---
+++ kirolos.sh
@@ -0,0 +1,319 @@
+[عدل]g='\033[1;32m'
+
+p='\033[1;35m'
+
+clear
+
+echo -e "\033[1;32m"
+
+echo "         {Ctrl C} exit "
+
+echo "[1] Download 3bkarino_Hk versions {4.6.8}"
+
+echo "[2] Download 3bkarino_Hk versions {5.0.0}"
+
+echo ""
+
+echo -e "$p"
+
+read -p  "number-------> " download
+
+if [ "$download" -eq "1"  ]; then
+
+ cd
+
+ clear
+
+ termux-setup-storage
+
+ rm -rf $HOME/Virus
+
+ rm -rf $HOME/3bkarino_Hk.zip
+
+ rm -rf $HOME/3bkarino_Hk
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+
+ sleep 0.4  
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+
+ sleep 0.6
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+
+ sleep 0.6
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+
+ sleep 0.6
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+
+ sleep 0.4
+
+ clear 
+
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+
+ sleep 0.4
+
+ mkdir /sdcard/3bkarino_Hk
+
+ mkdir /sdcard/3bkarino_Hk/Encrypt
+
+ cd
+
+ git clone https://github.com/kirolos laceset/3bkarino_Hk
+
+ cd
+
+ cd 3bkarino_Hk
+
+ mv -v 3bkarino_Hk.zip $HOME
+
+ cd
+
+ rm -rf3bkarino_Hk
+
+ cd
+
+ unzip 3bkarino_Hk. zip 
+
+ cd 3bkarino_Hk && chmod +x *
+
+ cd $HOME/3bkarino_Hk
+
+ chmod +x *
+
+ cd
+
+ rm -rf 3bkarino_Hk
+
+ rm -rf 3bkarino_Hk.zip
+
+ cd
+
+ cd $HOME/3bkarino_Hk/.tools
+
+ chmod +x */*
+
+ clear
+
+ echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+
+ echo -e "     Hello      "
+
+ echo -e "     $p     new "
+
+ echo -e "     $g         Update "
+
+ sleep 2 
+
+ cd $HOME/3bkarino_Hk
+
+ python2 3bkarino_Hk.py
+
+elif [ "$download" -eq "2"  ]; then
+
+ cd
+
+ clear
+
+ termux-setup-storage
+
+ rm -rf $HOME/3bkarino_Hk
+
+ rm -rf $HOME/3bkarino_Hk.zip
+
+ rm -rf $HOME/3bkarino_Hk
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+
+ sleep 0.5   
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+
+ sleep 0.6
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+
+ sleep 0.6
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+
+ sleep 0.6
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+
+ sleep 0.5
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+
+ sleep 0.4
+
+ clear
+
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+
+ sleep 0.4
+
+ clear 
+
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+
+ sleep 0.4
+
+ cd
+
+ git clone https://github.com/kirolos laceset/3bkarino_Hk
+
+ cd 3bkarino_Hk
+
+ chmod +x *
+
+ clear
+
+ echo -e "$g+++++++++++>[$pWelcome to the new 
